3cba7fd9e8c62365707f21a529f2d7730ca9bbb3
liyuan <864435965@qq.com>
2016-05-05 00:37:16 +0800

    first commit

---
+++ chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,30 @@
+package org.smart4j.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by LY on 2016/5/4.
+ */
+@WebServlet(name = "HelloServlet",urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.doGet(request,response);
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime=dateFormat.format(new Date());
+        request.setAttribute("currentTime",currentTime);
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,response);
+    }
+}
